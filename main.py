f79058c5d4d8058984208cb38bccb8bf6d0a2b82
dev <302685723@student.rocmondriaan.nl>
2024-09-27 10:10:34 +0200

    cache clearing

--- main.py
+++ main.py
@@ -3,17 +3,26 @@ import sys
 import dload
 
 n = len(sys.argv)
-root = "C:\\Users\\Kuray\\Documents\\git\\aclt"
+root = os.getcwd()
 
 def init_prog():
     if os.path.exists(root):
-        dload.save_unzip("https://github.com/JustKurayy/azelea-test/releases/download/V1.0.8/azelea-template.zip", root)
+        dload.save_unzip("https://github.com/JustKurayy/azelea-test/releases/download/V1.0.2/azelea-template.zip", root)
 
 def main():
     match sys.argv[1]:
         case "project:init":
             init_prog()
             return
+        case "cache:clear":
+            cwd = os.path.dirname(os.path.realpath(__file__))
+            filename = cwd + "\\azelea-template.zip"
+            if os.path.exists(filename):
+                os.remove(filename)
+                return
+            else:
+                print("Cache is already empty")
+                return
         case _:
             print("no argument passed!")
             return
